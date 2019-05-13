57dca4dd4151eecb7cc3b7bd2617f460bd6580b7
abdeladim abid <abdeladim.abid@gmail.com>
2019-05-13 12:03:26 +0000

    adding new styles

---
+++ update.php
@@ -0,0 +1,193 @@
+<?php
+include "database.php";
+$db = "store";
+$con = connect_db($root, $username, $password, $db);
+$query2 = "SELECT * FROM categories";
+$id = $_GET['id'];
+
+
+if (isset($_GET['id'])) {
+    $query = "SELECT * FROM items where item_id = " . $_GET['id'] . ";";
+    $result = mysqli_query($con, $query);
+    $row1 = mysqli_fetch_array($result);
+}
+
+if(isset($_POST['update'])) {
+    // $query3 = "UPDATE items SET name= ".$_POST['name'].",description=".$_POST['desc'].",price=".$_POST['prix'].",categorie=".$_POST['cat']." WHERE item_id=".$id.";";
+    // if($result = mysqli_query($con, $query3)) {
+    //     header("location: ./admin.php");
+    // }
+    // else {
+    //     echo "<script>alert('error !!! not updated')</script>";
+    // }
+
+        $name= $_POST["name"];
+$desc= $_POST["desc"];
+$price= $_POST["prix"];
+$cat= $_POST["cat"];
+	
+$target_dir = "upload/";
+$target_file = $target_dir . basename($_FILES["uploadedfile"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$message="";
+// Check if image file is a actual image or fake image
+	
+    $check = getimagesize($_FILES["uploadedfile"]["tmp_name"]);
+    if($check !== false) {
+		$message = "File is an image - " . $check["mime"];
+		$uploadOk = 1;
+    } else {
+		$message="File is not an image - " . $check["mime"];
+        $uploadOk = 0;
+    }
+// Check if file already exists
+if (file_exists($target_file)) {
+    $message= "
+  		Sorry, file already exist";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["uploadedfile"]["size"] > 500000) {
+    $message= "
+  		Sorry, your file is too large";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    $message= "
+  		Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+	    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "
+	<div style='text-align:center' class='alert alert-warning' role='alert'>
+  		Sorry, your file was not uploaded. ".$message."</div>";
+	// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["uploadedfile"]["tmp_name"], $target_file)) {
+        echo "
+		<div style='text-align:center' class='alert alert-success' role='alert'>
+          The file ". basename( $_FILES["uploadedfile"]["name"]). ". has been uploaded. ".$message."</div>";
+          
+          unlink($row1['img']);
+		
+		$url= "upload/".basename( $_FILES["uploadedfile"]["name"]);
+		
+		$query3 = "UPDATE items SET name='$name',description='$desc',price=$price,cat_id=$cat, img='$url' WHERE item_id=$id";
+		if (mysqli_query($con, $query3)) {
+			echo "New record created successfully";
+		} else {
+			echo "Error: " . $query3 . "<br>" . mysqli_error($con);
+		}
+
+$con->close();
+header('Location: ./admin.php');	
+
+    } else {
+        echo "
+		<div style='text-align:center' class='alert alert-warning' role='alert'>
+  		Sorry, there was an error uploading your file.</div>";
+	    }
+	}
+}
+if(isset($_POST['back'])) {
+    header("location: ./admin.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Reem+Kufi" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link rel="stylesheet" href="style(2).css">
+    <title>ADMIN PANEL</title>
+</head>
+
+<body>
+    <div class="full_container">
+        <div class="header">
+            <img id="deco" src="img/relax.png" alt="icon">
+            <h1>متجر العلم النافع</h1>
+            <img src="img/relax.png" alt="icon">
+        </div>
+        <div class="content">
+            <div class="car">
+                <div class="slam1">
+                    <form id="update_form" method="post" enctype="multipart/form-data">
+                        <h1>Modifier un item:</h1>
+                        <div class='form-group'>
+                            <label for='hassan'>Nom:</label>
+                            <input type='Name' class='form-control' name='name' id='exampleFormControlInput1' value='<?php echo $row1['name'] ?>'>
+                        </div>
+                        <div class='form-group'>
+                            <label for='Description'>Description</label>
+                            <input type='text' class='form-control' id='exampleFormControlInput1' name='desc' value='<?php echo $row1['description'] ?>'>
+                        </div>
+                        <div class='form-group'>
+                            <label for='Prix'>Prix: (en DH)</label>
+                            <input type='number' class='form-control' id='exampleFormControlInput1' name='prix' value='<?php echo $row1['price'] ?>'>
+                        </div>
+                        <div class="form-group">
+                            <label for="Tous les livres">Catégorie</label>
+                            <select class="form-control" id="exampleFormControlSelect1" name="cat">
+                                <?php
+                                if ($result = mysqli_query($con, $query2)) {
+                                    while ($row = mysqli_fetch_array($result)) {
+                                        echo "<option id='cat" . $row['cat_id'] . "' value='" . $row['cat_id'] . "'>" . $row['name'] . "</option>";
+                                    }
+                                }
+                                ?>
+
+                            </select>
+                        </div>
+                        <div class='form-group' style="display:inline">
+                            <label style="display:inline">Image: </label>
+                            <p style="color:white; display:inline"><?php echo $row1['img'] ?></p>
+                        </div>
+                        <div class="input">
+                        <br><label for="avatar">Sélectionner une image:</label>
+                            <input type="file" id="avatar" name="uploadedfile" accept="image/png, image/jpeg">
+                        </div>
+                        <div class=" button">
+                            <button type="submit" class="btn btn-success btn-lg" name="update"><i class='fas fa-pen'></i>&nbsp;&nbsp;Modifier</button>
+                            <button type="submit" class="btn btn-primary btn-lg" name="back"><i class="fas fa-arrow-left"></i>&nbsp;&nbsp;Retour</button>
+                        </div>
+                    </form>
+                </div>
+                <div class="slam2" style="margin-top: 2vw;">
+                    <form id="card_form">
+                        <div class='card' >
+                            <img class='card-img-top' src=<?php echo $row1['img'] ?> alt='Card image cap'>
+                            <div class='card-img-overlay'>
+                                <a href='#' class='btn btn-warning btn-lg deactivate' id='price' disabled><?php echo $row1['price'] ?> dhs</a>
+                            </div>
+                            <div class='card-body'>
+                                <h1><?php echo $row1['name'] ?></h1>
+                                <h2><?php echo $row1['description'] ?></h2>
+                            </div>
+                            <div class="card-footer">
+                                <button type='button' id='command' class='btn btn-warning btn-lg '>Commander</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
